7dcf20aff45396a3831a344ae7b764d03c97470e
Megha Ramesh <megharame@gmail.com>
2026-08-08 17:14:23 -0700

    Refactor storage into a module (state migrated, verified clean)

---
+++ infra/modules/storage/main.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+
+resource "azurerm_storage_account" "mob_storage" {
+  name                     = "mobtrainingstorage"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "software_engineering_docs" {
+  name                  = "software-engineering-docs"
+  storage_account_name  = azurerm_storage_account.mob_storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "company_docs" {
+  name                  = "company-docs"
+  storage_account_name  = azurerm_storage_account.mob_storage.name
+  container_access_type = "private"
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.mob_storage.name
+}
+
+output "storage_account_id" {
+  value = azurerm_storage_account.mob_storage.id
+}
